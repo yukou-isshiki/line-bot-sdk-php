3a1e2e1af77bbc13c1422a2678d11a92a19b8591
一色優孝 <yff24517@nifty.com>
2017-07-23 14:03:28 +0900

    不正であれば例外の内容を出力を削除

--- index.php
+++ index.php
@@ -19,7 +19,6 @@ $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => getenv('CHANNEL_SECRET
 $signature = $_SERVER['HTTP_' . \LINE\LINEBot\Constant\HTTPHeader::LINE_SIGNATURE];
 
 // 署名が正当かチェック。正当であればリクエストをパースし配列へ
-$events = $bot->parseEventRequest(file_get_contents('php://input'), $signature);
 // 不正であれば例外の内容を出力
 try {
   $events = $bot->parseEventRequest(file_get_contents('php://input'), $signature);
@@ -33,7 +32,7 @@ try {
   error_log('parseEventRequest failed. InvalidEventRequestException => '.var_export($e, true));
 }
 // 配列に格納された各イベントをループで処理
-foreach ((array)$events as $events) {
+foreach ($events as $event) {
   // MessageEventクラスのインスタンスでなければ処理をスキップ
   if(!($event instanceof \LINE\LINEBot\Event\MessageEvent)) {
     error_log('Non message event has come');
